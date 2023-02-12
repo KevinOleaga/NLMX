07b0df5b882ee25befd171b31c5151f34b9b9c1b
Kevin Oleaga <oleaga@outlook.com>
2023-02-12 11:45:16 -0600

    fix

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 const cors = require("cors");
 
-//Insert data in user collection and getting from API and
 app.use(express.json());
-// 3: we need to register our router 
 app.use(cors());
 app.use(users);
 app.use(data);
