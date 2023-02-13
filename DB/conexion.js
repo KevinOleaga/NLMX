e84c723f06ad2111680be788adbd31229ce1a44d
Kevin Oleaga <oleaga@outlook.com>
2023-02-13 16:25:15 -0600

    commit

--- DB/conexion.js
+++ DB/conexion.js
@@ -4,5 +4,5 @@ const DB = "mongodb://127.0.0.1:27017/Reto2"
 mongoose.connect(DB).then(() => {
     console.log(" - [OK] DB: Conexion exitosa!")
 }).catch((e) => {
-    console.log(" - [OK] FAIL: Conexion fallida > " + e);
+    console.log(" - [FAIL] FAIL: Conexion fallida > " + e);
 });
